efee06de53bc91f5cda8c7a17500584954efa71f
Pavel Shirshov <pshirshov@eml.cc>
2024-10-28 15:36:47 +0000

    wip: nixify

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+self="$(realpath "$0")"
+path="$(dirname "$self")"
+
+(for e in "$@"; do [[ "$e" == "nix" ]] && exit 0; done) && NIXIFY=1 || NIXIFY=0
+
+if [[ "$NIXIFY" == 1 && -z "${IN_NIX_SHELL+x}" ]]; then
+    echo "Restarting in Nix..."
+    set -x
+    nix flake lock
+    nix flake metadata
+    exec nix develop \
+      --ignore-environment \
+      --keep SONATYPE_SECRET \
+      --keep SCALA_VERSION \
+      --keep JDK_VERSION \
+      --keep CI_BRANCH \
+      --keep CI_COMMIT \
+      --keep CI_BRANCH_TAG \
+      --keep CI_PULL_REQUEST \
+      --keep CI_BUILD_UNIQ_SUFFIX \
+      --keep CI \
+      --keep HOME \
+      --command bash "$self" "$@"
+fi
+
+set -x
+cd "$path"
+
+for i in "$@"
+do
+case $i in
+    nix) ;;
+    env) exec bash -norc ;;
+    *) "./devops/$i.sh" ;;
+esac
+done
