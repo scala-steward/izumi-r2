cf3b0b210e67dd981610d70f273a1058205e587f
Pavel Shirshov <pshirshov@eml.cc>
2025-08-08 19:49:16 +0100

    proper version parser

--- fundamentals/fundamentals-platform/src/main/scala/izumi/fundamentals/platform/resources/ArtifactVersion.scala
+++ fundamentals/fundamentals-platform/src/main/scala/izumi/fundamentals/platform/resources/ArtifactVersion.scala
@@ -2,7 +2,6 @@ package izumi.fundamentals.platform.resources
 
 import izumi.fundamentals.platform.versions.Version
 
-// TODO: full-scale version, with proper parsing & comparators
 case class ArtifactVersion(version: Version) {
   override def toString: String = version.toString
 }
