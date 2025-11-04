8b95ed41e075abe0e1ac2e393a61f3dc98f207b5
Kai <450507+neko-kai@users.noreply.github.com>
2025-11-04 20:50:12 +0000

    Document that AbstractDIConfigReader is a workaround for https://github.com/scala/scala3/issues/24330

--- distage/distage-extension-config/src/main/scala/izumi/distage/config/codec/AbstractDIConfigReader.scala
+++ distage/distage-extension-config/src/main/scala/izumi/distage/config/codec/AbstractDIConfigReader.scala
@@ -5,6 +5,7 @@ import izumi.reflect.Tag
 
 import scala.util.Try
 
+/** This trait exists only to work around a Scala 3 bug #24330 https://github.com/scala/scala3/issues/24330 */
 private[codec] trait AbstractDIConfigReader[A] {
   def decodeConfig(config: DistageConfigImpl): Try[A]
   def decodeConfig(path: String)(config: DistageConfigImpl)(implicit tag: Tag[A]): A
