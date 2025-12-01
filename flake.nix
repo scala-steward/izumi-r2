ae47748803003f30d689d3eb6ca8d2ce94ebf918
Paul S. <pshirshov@eml.cc>
2025-12-01 00:28:33 +0000

    Switched to mudyla (#2314)

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
   description = "izumi build environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.11";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.mudyla.url = "github:7mind/mudyla";
+  inputs.mudyla.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
+    , mudyla
     ,
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -30,6 +34,8 @@
             gitMinimal
             openssh
 
+            mudyla.packages.${system}.default
+
             docker
             scala-cli
           ];
@@ -37,7 +43,8 @@
           shellHook = ''
             export JDK11=${pkgs.jdk11_headless}
             export JDK17=${pkgs.jdk17_headless}
-            export JDK22=${pkgs.jdk22_headless}
+            export JDK21=${pkgs.jdk21_headless}
+            export JDK25=${pkgs.jdk25_headless}
           '';
         };
       }
